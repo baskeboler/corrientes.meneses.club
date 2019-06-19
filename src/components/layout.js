0267065dd349f0e3c59e1ef9a8da934fba54236d
Victor Gil <baskeboler@gmail.com>
2019-06-18 21:15:49 -0300

    seo

--- src/components/layout.js
+++ src/components/layout.js
@@ -25,14 +25,14 @@ const Layout = ({ children, darkText }) => (
             { name: 'description', content: 'Corrientes y Meneses' },
             { name: 'keywords', content: 'site, web' },
             { name: 'image', content: `${config.siteUrl}${config.siteLogo}` },
+            { name: 'twitter:card', content: 'summary_large_image' },
+            { name: 'twitter:creator', content: '@baskeboler' },
             { name: 'twitter:title', content: config.siteTitle },
             { name: 'twitter:description', content: 'Corrientes y Meneses' },
-            { name: 'twitter:card', content: 'summary_large_image' },
             {
               name: 'twitter:image',
               content: `${config.siteUrl}${config.siteLogo}`,
             },
-            { name: 'twitter:creator', content: '@baskeboler' },
             { name: 'title', content: config.siteTitle },
             { name: 'og:description', content: 'Corrientes y Meneses' },
             { name: 'og:title', content: config.siteTitle },
@@ -40,6 +40,8 @@ const Layout = ({ children, darkText }) => (
               name: 'og:image',
               content: `${config.siteUrl}${config.siteLogo}`,
             },
+            { name: 'og:type', content: 'website' },
+            { name: 'og:url', content: config.siteUrl },
           ]}
         >
           <html lang="en" />
